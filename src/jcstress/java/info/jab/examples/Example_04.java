28a8c5012ff947e3b35f94401ce4c6fd772ab498
Juan Antonio Breña Moral <bren@juanantonio.info>
2022-08-15 13:16:26 +0200

    Adding new slides

--- src/jcstress/java/info/jab/examples/Example_04.java
+++ src/jcstress/java/info/jab/examples/Example_04.java
@@ -1,31 +1,81 @@
 package info.jab.examples;
 
 import org.openjdk.jcstress.annotations.Actor;
+import org.openjdk.jcstress.annotations.Expect;
 import org.openjdk.jcstress.annotations.JCStressTest;
+import org.openjdk.jcstress.annotations.Mode;
 import org.openjdk.jcstress.annotations.Outcome;
+import org.openjdk.jcstress.annotations.Signal;
 import org.openjdk.jcstress.annotations.State;
-import org.openjdk.jcstress.infra.results.II_Result;
 
-import static org.openjdk.jcstress.annotations.Expect.ACCEPTABLE;
-import static org.openjdk.jcstress.annotations.Expect.ACCEPTABLE_INTERESTING;
-
-@JCStressTest
-@Outcome(id = {"0, 1", "1, 0", "1, 1"}, expect = ACCEPTABLE, desc = "ok")
-@Outcome(id = "0, 0", expect = ACCEPTABLE_INTERESTING, desc = "danger")
-@State
 public class Example_04 {
 
-    int x, y;
+    @JCStressTest(Mode.Termination)
+    @Outcome(id = "TERMINATED", expect = Expect.ACCEPTABLE, desc = "Gracefully finished.")
+    @Outcome(id = "STALE", expect = Expect.ACCEPTABLE_INTERESTING, desc = "Test hung up.")
+    @State
+    public static class Case5 {
 
-    @Actor
-    public void actor1(II_Result r) {
-        x = 1;
-        r.r2 = y;
+        AuxCase5 v = new AuxCase5();
+
+        @Actor
+        public void actor1() {
+            v.poll();
+        }
+
+        @Signal
+        public void actor2() {
+            v.enable();
+        }
     }
 
-    @Actor
-    public void actor2(II_Result r) {
-        y = 1;
-        r.r1 = x;
+    public static class AuxCase5 {
+
+        boolean flag = false;
+
+        public void enable() {
+            flag = true;
+        }
+
+        public void poll() {
+            while(flag == false) {
+                //Spin
+            }
+        }
     }
+
+    @JCStressTest(Mode.Termination)
+    @Outcome(id = "TERMINATED", expect = Expect.ACCEPTABLE, desc = "Gracefully finished.")
+    @Outcome(id = "STALE", expect = Expect.ACCEPTABLE_INTERESTING, desc = "Test hung up.")
+    @State
+    public static class Case6 {
+
+        AuxCase6 v = new AuxCase6();
+
+        @Actor
+        public void actor1() {
+            v.poll();
+        }
+
+        @Signal
+        public void actor2() {
+            v.enable();
+        }
+    }
+
+    public static class AuxCase6 {
+
+        volatile boolean flag = false;
+
+        public void enable() {
+            flag = true;
+        }
+
+        public void poll() {
+            while(flag == false) {
+                //Process
+            }
+        }
+    }
+
 }
